ca952fb715260fc4c0e58f8235232ffb7c0e1c82
dayangac <emreday01@gmail.com>
2026-09-20 23:07:53 +0100

    Drop unreferenced kirigami_tessellations baseline files (1/3)

--- baseline/kirigami_tessellations/code/cpp/json.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:551264c2c087999d3f7a43be04b31935f3459a56f3466f7b5acecde5e541e7b5
-size 956133
--- baseline/kirigami_tessellations/code/cpp/parameterization/param.h
+++
@@ -1,7 +0,0 @@
-#pragma  once
-#include <Eigen/Eigen>
-#include "../Hmesh.h"
-
-namespace param {
-  Eigen::MatrixXd isometric_param(utils::Hmesh& mesh);
-}
